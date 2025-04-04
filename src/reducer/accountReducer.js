2bf506439bc031c11d5fb0e87afc3b1986e36094
innazinchenko <spangle_red@inbox.ru>
2025-04-04 22:36:19 +0200

    3 commit

--- src/reducer/accountReducer.js
+++ src/reducer/accountReducer.js
@@ -8,5 +8,7 @@ export const accountReducer = (state, action) => {
             return {...state, balance: state.balance + action.payload};
         case WITHDRAW:
             return {...state, balance: state.balance < action.payload ? state.balance : state.balance - action.payload};
+        default:
+            return state;
     }
 }
